053394e35a0ae157316bd827157f52d7bb4d8c33
Abdellatif MSADIA <62403061+Abdoulenet@users.noreply.github.com>
2021-04-09 12:35:42 +0100

    Update 2

---
+++ JsonConvert.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewtonSoft
+{
+    class JsonConvert<T>
+    {
+        JsonSerializer serializer = new JsonSerializer();
+
+        public object DeserializeObject(string param)
+        {
+            return JsonConvert.DeserializeObject(param);
+        }
+
+        public object Deserialize(JsonReader param)
+        {
+            return serializer.Deserialize(param);
+        }
+    }
+}
